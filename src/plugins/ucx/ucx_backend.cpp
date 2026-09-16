c71f376cd55c7d1a0ed4206bded4180de9475a0d
Tomer Davidor <tdavidor@nvidia.com>
2026-09-16 15:46:50 +0300

    ucx: synchronize base engine notification callbacks and drains

--- src/plugins/ucx/ucx_backend.cpp
+++ src/plugins/ucx/ucx_backend.cpp
@@ -1038,7 +1038,8 @@ nixlUcxEngine::getConnection(const std::string &remote_agent) const {
 
 void
 nixlUcxEngine::appendNotif(std::string &&remote_name, std::string &&msg) {
-    // In the "no progress thread" case the lock in nixlAgent is sufficient.
+    // Proxy progress can invoke this callback outside the agent lock.
+    const std::lock_guard lock(baseNotifMutex_);
     notifList_.emplace_back(std::move(remote_name), std::move(msg));
 }
 
@@ -1073,7 +1074,7 @@ nixlUcxEngine::getNotifs(notif_list_t &notif_list) {
 
     progressLoop();
 
-    // In the "no progress thread" case the lock in nixlAgent is sufficient.
+    const std::lock_guard lock(baseNotifMutex_);
     notifList_.swap(notif_list);
     return NIXL_SUCCESS;
 }
